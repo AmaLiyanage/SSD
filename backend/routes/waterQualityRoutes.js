d0edb515ebf660d0dab9c1140a3ef0d50a8f6b35
lewkes <ruwanyasathmi09@gmail.com>
2026-09-23 10:40:18 +0530

    Fix Vulnerability 1-Missing Authentication on Lab Report API

--- backend/routes/waterQualityRoutes.js
+++ backend/routes/waterQualityRoutes.js
@@ -8,14 +8,21 @@ import {
     deleteTestResult 
 } from '../controllers/waterQualityController.js';
 
+import { protect } from '../middleware/authMiddleware.js'; 
+//project's middleware exports protect
+
 const router = express.Router();
 
+
+
 // Defined routes for Water Quality Monitoring
-router.get('/', getAllTests);
-router.get('/well/:wellId', getWellHistory);
-router.get('/:id', getTestById);
-router.post('/', addTestResult);
-router.put('/:id', updateTestResult);
-router.delete('/:id', deleteTestResult);
+router.get('/', protect, getAllTests);
+//router.get('/', getAllTests); 
+
+router.get('/well/:wellId', protect, getWellHistory);
+router.get('/:id', protect, getTestById);
+router.post('/', protect, addTestResult);
+router.put('/:id', protect, updateTestResult);
+router.delete('/:id', protect, deleteTestResult);
 
 export default router;
